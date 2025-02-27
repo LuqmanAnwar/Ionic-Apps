4887540ef298351b41fa43453d9fdad8b3b4da35
Luqman Nurhakim <nurhakim2201@gmail.com>
2025-02-27 01:55:41 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'KPDN_APPS',
+  webDir: 'dist'
+};
+
+export default config;
